07fc6aec6ba95b3f48c60e59129ecd298ac9ed76
MD Gaziur Rahman Noor <mdgaziur001@ferrisbox.localdomain>
2021-07-28 18:59:38 +0600

    Project: Initial

---
+++ server/src/main.rs
@@ -0,0 +1,11 @@
+use actix_web::{HttpServer, App};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
