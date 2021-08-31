d5338817718dcbf535879a424b1476ca1665a8ad
uthsobcb <uthsob9@gmail.com>
2021-09-01 01:10:23 +0600

    Added Head Part

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<div id="header-navbar-bottom">
+    <router-link to="/">/ </router-link>  
+    <router-link to="/header"> Blogs</router-link> 
+    <router-link to="/header"> Workshops</router-link> 
+    <router-link to="/about"> About Us</router-link>
+  </div>
+  <router-view/>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+
+		<div id="header-navbar">
+			<div style="float:right">
+				<a href="">Login</a> || 
+				<button><a href="">Join Us</a></button>
+			</div>
+        </div>
+<div id="logo-container" style="margin-top: 4%;">
+			<div class="separator">
+				<div>
+					<h1 id="nerdtree">We're NerdTree</h1>
+					<p>An organization aiming to make a better programming community for students</p>
+				</div>
+			</div>
+			<div class="separator">
+				<img id="logo" src="../assets/logo.png">
+			</div>
+		</div>
+
+</template>
+
+<script lang="ts">
+ 
+</script>
+<style lang="sass">
+
+</style>
