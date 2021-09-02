76f2d252825f8d9350c90e43f144f88c664b459b
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-09-02 18:16:31 +0600

    Frontend: Start again :)

---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>NerdTree Web Infrastructure</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div id="header-navbar-bottom">
-    <router-link to="/">/ </router-link>  
-    <router-link to="/header"> Blogs</router-link> 
-    <router-link to="/header"> Workshops</router-link> 
-    <router-link to="/about"> About Us</router-link>
-  </div>
-  <router-view/>
-</template>
