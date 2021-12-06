525e5480352820fc4a0eae0f7c55d04e802d27de
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-12-06 20:45:35 +0600

    API Wrapper: Add wrappers for /comment route

---
+++ nerdtree-frontend/api-wrapper/comment/query/by_user.ts
@@ -0,0 +1,48 @@
+import NerdTreeSession from '~/store/session'
+import { StatusPayload } from '~/api-wrapper/common'
+import { PaginatedComments } from '~/api-wrapper/comment'
+
+export default async function ByUser(
+  authorId: String,
+  page: Number,
+  perPage: Number,
+  ctx: NerdTreeSession
+): Promise<StatusPayload<PaginatedComments>> {
+  const payload = {
+    author_id: authorId,
+    page,
+    per_page: perPage
+  }
+
+  try {
+    const req = await fetch(`${process.env.NERDTREE_API_URL}/comment/query/by_user`, {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${ctx.AccessToken}`
+      },
+    });
+
+    const jsonBody = await req.json();
+
+    if (req.status !== 200) {
+      return {
+        success: false,
+        message: jsonBody.error,
+      }
+    }
+    else {
+      return {
+        success: true,
+        value: jsonBody
+      }
+    }
+  }
+  catch {
+    return {
+      success: false,
+      message: "Cannot connect to server",
+    }
+  }
+}
---
+++ nerdtree-frontend/api-wrapper/comment/update/edit.ts
@@ -0,0 +1,46 @@
+import NerdTreeSession from '~/store/session'
+import { StatusPayload } from '~/api-wrapper/common'
+import { Comment } from '~/api-wrapper/comment'
+
+export default async function Edit(
+  commentId: String,
+  body: String,
+  ctx: NerdTreeSession
+): Promise<StatusPayload<Comment>> {
+  const payload = {
+    comment_id: commentId,
+    body,
+  }
+
+  try {
+    const req = await fetch(`${process.env.NERDTREE_API_URL}/comment/update/edit`, {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${ctx.AccessToken}`
+      },
+    });
+
+    const jsonBody = await req.json();
+
+    if (req.status !== 200) {
+      return {
+        success: false,
+        message: jsonBody.error,
+      }
+    }
+    else {
+      return {
+        success: true,
+        value: jsonBody
+      }
+    }
+  }
+  catch {
+    return {
+      success: false,
+      message: "Cannot connect to server",
+    }
+  }
+}
