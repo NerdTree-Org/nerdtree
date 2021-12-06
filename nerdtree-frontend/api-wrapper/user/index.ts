1b0d8bb9165c3f76e77a8db1be7c0931df3a4f0b
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-12-06 13:06:49 +0600

    API Wrapper: Add wrapper for /user routes

---
+++ nerdtree-frontend/api-wrapper/user/index.ts
@@ -0,0 +1,47 @@
+import Firstname from '~/api-wrapper/user/update/firstname'
+import Lastname from '~/api-wrapper/user/update/lastname'
+import Email from '~/api-wrapper/user/update/email'
+import Password from '~/api-wrapper/user/update/password'
+import ProfilePic from '~/api-wrapper/user/update/profile_pic'
+import DeleteUser from '~/api-wrapper/user/update/delete'
+import ById from '~/api-wrapper/user/query/by_id'
+import ByEmail from '~/api-wrapper/user/query/by_email'
+import ByUsername from '~/api-wrapper/user/query/by_username'
+import Current from '~/api-wrapper/user/query/current'
+import DiscordToken from '~/api-wrapper/user/query/discord_token'
+
+export type User = {
+ id: String,
+ username: String,
+ firstname: String,
+ lastname: String,
+ email: String,
+  // eslint-disable-next-line camelcase
+ profile_pic: String | null,
+  // eslint-disable-next-line camelcase
+ is_admin: boolean,
+  // eslint-disable-next-line camelcase
+ facebook_id: String,
+  // eslint-disable-next-line camelcase
+ discord_token: String,
+  // eslint-disable-next-line camelcase
+ is_discord_token_used: boolean,
+}
+
+export default {
+  update: {
+    Firstname,
+    Lastname,
+    Email,
+    Password,
+    ProfilePic,
+    DeleteUser,
+  },
+  query: {
+    ById,
+    ByEmail,
+    ByUsername,
+    Current,
+    DiscordToken,
+  }
+}
---
+++ nerdtree-frontend/api-wrapper/user/query/by_id.ts
@@ -0,0 +1,44 @@
+import NerdTreeSession from '~/store/session'
+import { StatusPayload } from '~/api-wrapper/common'
+import { User } from '~/api-wrapper/user'
+
+export default async function ById(
+  userId: String,
+  ctx: NerdTreeSession
+): Promise<StatusPayload<User>> {
+  const payload = {
+    user_id: userId,
+  }
+
+  try {
+    const req = await fetch(`${process.env.NERDTREE_API_URL}/user/query/id`, {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${ctx.accessToken}`
+      },
+    });
+
+    const jsonBody = await req.json();
+
+    if (req.status !== 200) {
+      return {
+        success: false,
+        message: jsonBody.error,
+      }
+    }
+    else {
+      return {
+        success: true,
+        value: jsonBody
+      }
+    }
+  }
+  catch {
+    return {
+      success: false,
+      message: "Cannot connect to server",
+    }
+  }
+}
