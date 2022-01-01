2e365d7e2903bc39ad6784175ec4fd7629299d55
mdgaziur <mdgaziurrahmannoor@gmail.com>
2022-01-01 23:02:22 +0600

    Frontend: New project structure
    Remove old files and move api-wrapper and store to new structure

--- nerdtree-frontend/api-wrapper/blog/update/new_post.ts
+++ nerdtree-frontend/api-wrapper/blog/update/new_post.ts
@@ -18,29 +18,27 @@ export default async function CreateNewPost(
       body: JSON.stringify(payload),
       headers: {
         'content-type': 'application/json',
-        'authorization': `bearer ${ctx.AccessToken}`
+        authorization: `bearer ${ctx.AccessToken}`,
       },
-    });
+    })
 
-    const jsonBody = await req.json();
+    const jsonBody = await req.json()
 
     if (req.status !== 200) {
       return {
         success: false,
         message: jsonBody.error,
       }
-    }
-    else {
+    } else {
       return {
         success: true,
-        value: jsonBody
+        value: jsonBody,
       }
     }
-  }
-  catch {
+  } catch {
     return {
       success: false,
-      message: "Cannot connect to server",
+      message: 'Cannot connect to server',
     }
   }
 }
--- nerdtree-frontend/api-wrapper/blog/update/title.ts
+++ nerdtree-frontend/api-wrapper/blog/update/title.ts
@@ -10,37 +10,38 @@ export default async function Title(
   const payload = {
     post_id: postId,
     title,
-  };
+  }
 
   try {
-    const req = await fetch(`${process.env.NERDTREE_API_URL}/post/update/title`, {
-      method: 'post',
-      body: JSON.stringify(payload),
-      headers: {
-        'content-type': 'application/json',
-        'authorization': `bearer ${ctx.AccessToken}`
-      },
-    });
+    const req = await fetch(
+      `${process.env.NERDTREE_API_URL}/post/update/title`,
+      {
+        method: 'post',
+        body: JSON.stringify(payload),
+        headers: {
+          'content-type': 'application/json',
+          authorization: `bearer ${ctx.AccessToken}`,
+        },
+      }
+    )
 
-    const jsonBody = await req.json();
+    const jsonBody = await req.json()
 
     if (req.status !== 200) {
       return {
         success: false,
         message: jsonBody.error,
       }
-    }
-    else {
+    } else {
       return {
         success: true,
-        value: jsonBody
+        value: jsonBody,
       }
     }
-  }
-  catch {
+  } catch {
     return {
       success: false,
-      message: "Cannot connect to server",
+      message: 'Cannot connect to server',
     }
   }
 }
